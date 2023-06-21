c41701afd4cf80392afba45ced9b3f2fe03bd80e
Paulo Vitor Lessa <paulo.vitor.lessa@gmail.com>
2023-06-21 16:00:36 -0300

    axios base URL updated

--- src/api/AxiosInstance.js
+++ src/api/AxiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://192.168.1.4:8080/api",
+  baseURL: "https://apilivraria-production.up.railway.app/api",
 });
 
 export default AxiosInstance;
